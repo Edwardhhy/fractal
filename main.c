d0c3eeb47a2d4a893d17d97bdadf423baf8b22d5
amdiels <38467388+amdiels@users.noreply.github.com>
2018-05-07 11:02:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,17 @@
 
 int main()
 {
+    void* fileReading(char* filename){
+       fDes = open(filename,O_RDONLY) ;
+       if(fDes < 0){
+          perror("impssible de lire le fichier") ;
+          close(fDes) ;
+          exit(EXIT_FAILURE) ;
+       }
+       while(!feof(fDes)){
+          
+       }
+    }
+    return 0;
 
 }
